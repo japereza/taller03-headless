0dfc7ca73bba9683a0892d299caee36917cdc290
Javier Perez <ja.pereza@uniandes.edu.co>
2020-02-17 23:29:26 -0500

    evidenciar taller 03

---
+++ puppeteer/__tests__/test1.js
@@ -0,0 +1,22 @@
+const timeout = 10000
+
+describe(
+  '/ (Home Page)',
+  () => {
+    let page
+    beforeAll(async () => {
+      page = await global.__BROWSER__.newPage()
+      await page.goto('https://www.eltiempo.com')
+    }, timeout)
+
+    afterAll(async () => {
+      await page.close()
+    })
+
+    it('should load without error', async () => {
+      let text = await page.evaluate(() => document.body.textContent)
+      expect(text).toContain('Tiempo')
+    })
+  },
+  timeout
+)
